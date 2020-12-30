18c0f95e5b436392c554ed5cad0863ddcd522477
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2020-12-30 16:33:56 +0200

    Speed

--- src/UdemyAcademy/Main_Speed_converter.java
+++ src/UdemyAcademy/Main_Speed_converter.java
@@ -5,5 +5,7 @@ public class Main_Speed_converter {
 	public static void main(String[] args) {
    double miles = SpeedConverted.toMilesPerHour(10.5);
    System.out.println("Miles " + miles);
+   
+   SpeedConverted.printConversion(80.9);
 	}
 }

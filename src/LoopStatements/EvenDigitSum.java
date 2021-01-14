12df5316ecff4274a75cda14dc1f8d00fbe45259
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-14 15:21:34 +0200

    EvenDigitSUm

---
+++ src/LoopStatements/EvenDigitSum.java
@@ -0,0 +1,5 @@
+package LoopStatements;
+
+public class EvenDigitSum {
+
+}
--- src/LoopStatements/Number_Palindrome.java
+++ src/LoopStatements/Number_Palindrome.java
@@ -1,5 +1,21 @@
 package LoopStatements;
 
 public class Number_Palindrome {
-
+	public static boolean isPalindrome(int number) {
+		int reverse =0;
+		int num = number;
+		
+		while(number!=0) {
+			int lastDigit = number %10;
+			reverse = reverse * 10;
+			reverse+=lastDigit;
+			number /= 10;
+		}
+		if(num == reverse) {
+			return true;
+	}else {
+		return false;
+	}
+	
+}
 }

73b8483125c7263c6fc44faf96e7c454d36a7957
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-18 13:58:17 +0200

    Year of Birth

--- src/UserInput/Main.java
+++ src/UserInput/Main.java
@@ -8,15 +8,27 @@ public class Main {
 		Scanner scanner = new Scanner (System.in);
 		
 		System.out.println("Enter your year of birth: ");
-		int yearOfBirth= scanner.nextInt();
-		scanner.nextLine();
 		
-		System.out.println("Enter your name: ");
+		boolean hasNextInt = scanner.hasNextInt();
 		
-		String name = scanner.nextLine();
-		
-		int age = 2021 - yearOfBirth;
-		System.out.println("Your name is " + name + ", and you are " + age + " years old");
+		if(hasNextInt) {
+			int yearOfBirth= scanner.nextInt();
+			scanner.nextLine();
+			
+			System.out.println("Enter your name: ");
+			
+			String name = scanner.nextLine();
+			
+			int age = 2021 - yearOfBirth;
+			
+			 if (age >= 0 && age <=100) {
+				 System.out.println("Your name is " + name + ", and you are " + age + " years old");
+			 }else {
+				 System.out.println("Invalid year of birth");
+			 }
+			}else {
+				System.out.println("Unable to parse year of birth");
+			}
 		scanner.close();		
 	}
 

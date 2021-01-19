4fa469b5fd4dbd8d34b3b40e29f114450b4b4393
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-19 13:03:50 +0200

    InputCalculator

--- src/UserInput/InputCalculator.java
+++ src/UserInput/InputCalculator.java
@@ -1,5 +1,32 @@
 package UserInput;
 
+import java.util.Scanner;
+
 public class InputCalculator {
+	public static void inputThenPrintSumAndAverage () {
+		Scanner sc =  new Scanner(System.in);
+		
+		int count = 0;
+		int sum = 0;
+		long average = 0L;
+		
+		 while(true) {
+			 boolean isAnInt= sc.hasNextInt();
+			 
+			 if(isAnInt) {
+				 int number = sc.nextInt();
+				 count ++;
+				 sum+=number;
+			 }else {
+				 break;
+			 }
+			 average = Math.round((double)sum/count);
+			 sc.hasNextLine();
+			 
+		 }
+		
+		System.out.println("SUM = " + sum + " AVG = " + average);
+		sc.close();
+	}
 
 }

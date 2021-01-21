3ad737de9e6620de41f690ca5e3aaae9ccf32342
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-21 19:56:33 +0200

    Wall

--- src/OOP1/Wall.java
+++ src/OOP1/Wall.java
@@ -2,4 +2,45 @@ package OOP1;
 
 public class Wall {
 
-}
+	private double width;
+	private double height;
+	
+	public Wall() {
+		
+	}
+	
+	public Wall(double width, double height) {
+		this.width = width;
+		this.height = height;
+	}
+		
+		public void setWidth(double width) {
+	        if(width < 0) {
+	            this.width = 0;
+	        }
+	        else
+	        {
+	            this.width = width;
+	        }
+	    }
+	 
+	    public void setHeight(double height) {
+	        if (height < 0) {
+	            this.height = 0;
+	        } else {
+	            this.height = height;
+	        }
+	    }
+	 
+	    public double getWidth() {
+	        return width;
+	    }
+	 
+	    public double getHeight() {
+	        return height;
+	    }
+	    public double getArea()
+	    {
+	        return (width*height);
+	 
+	    }}

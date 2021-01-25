3ab121b576f7d22e9389949c6860b6a86c7c6570
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-25 20:53:49 +0200

    Fish_extendsAnimal

--- src/InheritanceLesson/Fish.java
+++ src/InheritanceLesson/Fish.java
@@ -1,10 +1,42 @@
 package InheritanceLesson;
 
-public class Fish {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Fish extends Animal{
+	private int gills;
+	private int yeys;
+	private int fins;
+	
+	// create a constructor which add in () parameters from super class:
+	//(String name, int brain, int body, int size, int weight, int gills, int yeys, int fins)
+	
+	public Fish(String name, int size, int weight, int gills, int yeys, int fins) {
+		super(name, 1, 1, size, weight);
+		this.gills = gills;
+		this.yeys = yeys;
+		this.fins = fins;
 	}
 
+private void rest() {
 }
+
+private void moveMuscles() {
+	
+}
+private void moveBackFin() {
+
+}
+
+private void swim(int speed) {
+	moveMuscles();
+	moveBackFin();
+	super.move(speed);
+}
+
+}
+
+
+
+
+
+
+
+

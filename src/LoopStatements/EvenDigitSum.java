b2bcff9fa943386757e9fafd7789df245b09ae08
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-25 20:55:19 +0200

    Animal

--- src/LoopStatements/EvenDigitSum.java
+++ src/LoopStatements/EvenDigitSum.java
@@ -1,5 +1,23 @@
 package LoopStatements;
 
 public class EvenDigitSum {
+	public static int getEvenDigitSum (int number) {
+		
+		
+		if (number < 0) {
+			return -1;
+		}
+		int evenDigitSum = 0;
+		while( number > 0) {
+			if ((number % 2) == 0){
+			evenDigitSum += number%10;
+		}
+			number /=10;
+		}
+		
+		return evenDigitSum;
+	}
+	
 
 }
+
--- src/UdemyAcademy/EqualityPrinter.java
+++ src/UdemyAcademy/EqualityPrinter.java
@@ -1,5 +1,16 @@
 package UdemyAcademy;
 
 public class EqualityPrinter {
-
+	public static void printEqual (int x, int y, int h) {
+		if ((x < 0) || (y < 0) || (h < 0)) {
+			System.out.println("Invalid Value");
+		}else if (x==y && x == h && y == h) {
+			System.out.println("All numbers are equal");
+		}else if(x!=y && x != h && y != h) {
+		System.out.println("All numbers are different");
+		} else {
+        System.out.println("Neither all are equal or different");
+		}
+	}
 }
+

9a0a40898e16c5595a7e7d80c2b18447cca64ef6
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-27 18:19:57 +0200

    Cuboid

--- src/PoolArea/Cuboid.java
+++ src/PoolArea/Cuboid.java
@@ -1,5 +1,22 @@
 package PoolArea;
 
-public class Cuboid {
+public class Cuboid extends Rectangle {
+	private double height;
+
+	public Cuboid(double width, double length, double height) {
+		super(width, length);
+		if(height<0) {
+			this.height=0;
+		}else {
+		this.height = height;
+	}
+	}
+	
+	public double getHeight() {
+		return this.height;
+	}
+	public double getVolume () {
+		return getArea()*height;
+	}
 
 }

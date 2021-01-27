3c83adc106fa8bce7ed4b55ba46ea3685fca0086
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-27 17:46:09 +0200

    Circle

--- src/Cylinder/Circle.java
+++ src/Cylinder/Circle.java
@@ -1,5 +1,27 @@
 package Cylinder;
 
 public class Circle {
+	
+	private double radius;
 
+	public Circle(double radius) {
+		if(this.radius < 0) {
+			this.radius = 0;
+		}else {
+		
+			this.radius = radius;
+		
+	}
+	}
+public double getRadius() {
+	return this.radius;
 }
+	
+public double getArea() {
+	return (this.radius*this.radius*Math.PI);
+}
+	}
+
+	
+
+

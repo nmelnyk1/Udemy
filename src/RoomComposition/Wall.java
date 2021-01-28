82e8e9208b267acc22715b94dd68739dcdc059f3
nmelnyk <nmelnyk@NB00ORB068.N-IX.LOCAL>
2021-01-28 20:29:19 +0200

    Bedroom

---
+++ src/RoomComposition/Wall.java
@@ -0,0 +1,12 @@
+package RoomComposition;
+
+public class Wall {
+	private String direction;
+	
+public Wall(String direction) {
+	this.direction=direction;
+}
+public String gerDirection() {
+	return direction;
+}
+}
